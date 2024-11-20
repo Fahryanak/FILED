85bee9a36b754fff408daa9df74693754463a4f9
HANNZZG <160597974+Fahryanak@users.noreply.github.com>
2024-11-20 08:26:44 +0700

    update src/main/java/com/hansproduction/filed/FileDownloaderPlugin.java

--- src/main/java/com/hansproduction/filed/FileDownloaderPlugin.java
+++ src/main/java/com/hansproduction/filed/FileDownloaderPlugin.java
@@ -1,42 +1,35 @@
 package com.hansproduction.filed;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FileDownloaderPlugin extends JavaPlugin {
+import java.nio.file.Paths;
 
-    private FileDownloadManager fileDownloadManager;
-    private String defaultDownloadPath;
-    private double defaultSpeedLimit;
+public class FileDownloaderPlugin extends JavaPlugin {
+    private FileDownloadManager downloadManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        loadConfig();
-        fileDownloadManager = new FileDownloadManager(this, defaultDownloadPath, defaultSpeedLimit);
-        getCommand("filed").setExecutor(new FileCommand(this, fileDownloadManager));
-        getLogger().info("FileDownloader Plugin Enabled!");
+        this.downloadManager = new FileDownloadManager(this);
+
+        getCommand("filed").setExecutor(new FileCommand(this));
+        getLogger().info("FileDownloader plugin enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("FileDownloader Plugin Disabled!");
+        getLogger().info("FileDownloader plugin disabled!");
     }
 
-    public void loadConfig() {
+    public void reloadPluginConfig() {
         reloadConfig();
-        defaultDownloadPath = getConfig().getString("default-download-path", "downloads/");
-        defaultSpeedLimit = getConfig().getDouble("default-speed-limit", 1.0);
+        downloadManager.updateConfig(
+            getConfig().getLong("default-download-speed", 1),
+            Paths.get(getConfig().getString("default-download-path", "downloads"))
+        );
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("filed") && args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            loadConfig();
-            sender.sendMessage("Configuration reloaded!");
-            return true;
-        }
-        return false;
+    public FileDownloadManager getDownloadManager() {
+        return downloadManager;
     }
 }
